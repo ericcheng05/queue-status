fcd26d0c15c4d4c1b48a7a9b6aaed6dcc2f0bfa5
ericcheng05 <13010622+ericcheng05@users.noreply.github.com>
2022-08-20 00:39:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,59 @@
+<?php
+	$sushiroStoreList = "https://sushipass.sushiro.com.hk/api/2.0/info/storelist?latitude=22&longitude=114&numresults=25&region=HK";
+	$sushiroQueuePath = "https://sushipass.sushiro.com.hk/api/2.0/remote/groupqueues?region=HK&storeid=";
+	//  Initiate curl
+	$channel = curl_init();
+	curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($channel, CURLOPT_URL,$sushiroStoreList);
+	// Execute
+	$storeList = curl_exec($channel);
+	curl_close($channel);
+
+	$decodedStoreList = json_decode($storeList);
+	$storeCount = count($decodedStoreList);
+
+	for ($x = 0; $x < $storeCount; $x++)
+	{
+		echo $decodedStoreList[$x]->name;
+		echo " Current Queue Size: ".$decodedStoreList[$x]->waitingGroup;
+
+		$channel = curl_init();
+		curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($channel, CURLOPT_URL,$sushiroQueuePath.$decodedStoreList[$x]->id);
+		// Execute
+		$storeQueue = curl_exec($channel);
+		$decodedStoreQueue = json_decode($storeQueue);
+		echo "Next Ticket: ".$decodedStoreQueue->mixedQueue[0], PHP_EOL;
+		curl_close($channel);
+	}
+?>
+
 <html>
 	<head>
 		<title>Sushiro HK Store Queue Status</title>
 		<meta http-equiv="refresh" content="120">
 	</head>
 	<body>
-		<?php
-			$sushiroStoreList = "https://sushipass.sushiro.com.hk/api/2.0/info/storelist?latitude=22&longitude=114&numresults=25&region=HK";
-			$sushiroQueuePath = "https://sushipass.sushiro.com.hk/api/2.0/remote/groupqueues?region=HK&storeid=";
-			//  Initiate curl
-			$channel = curl_init();
-			curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($channel, CURLOPT_URL,$sushiroStoreList);
-			// Execute
-			$storeList = curl_exec($channel);
-			curl_close($channel);
-
-			$decodedStoreList = json_decode($storeList);
-			$storeCount = count($decodedStoreList);
-			echo "breakpoint";
-			for ($x = 0; $x < $storeCount; $x++)
-			{
-				echo $decodedStoreList[$x]->name;
-				echo " Current Queue Size: ".$decodedStoreList[$x]->waitingGroup;
-    				
-				$channel = curl_init();
-				curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($channel, CURLOPT_URL,$sushiroQueuePath.$decodedStoreList[$x]->id);
-				// Execute
-				$storeQueue = curl_exec($channel);
-				$decodedStoreQueue = json_decode($storeQueue);
-				echo "Next Ticket: ".$decodedStoreQueue->mixedQueue[0], PHP_EOL;
-				curl_close($channel);
-			}
-		
-	?>
-  </body>
+		<table>
+			<tr>
+				<th>Store</th>
+				<th>Current Queue Size</th>
+				<th>Next 1st Ticket</th>
+				<th>Next 2nd Ticket</th>
+				<th>Next 3rd Ticket</th>
+  			</tr>
+			<?php
+				foreach ($decodedStoreList as $value) 
+				{
+					echo "<tr>";
+					echo "<td>$value->name</td>";
+					echo "<td>$value->waitingGroup</td>";
+					echo "<td>0</td>";
+					echo "<td>1</td>";
+					echo "<td>2</td>";
+					echo "</tr>";
+				}
+			?>
+		</table>
+	</body>
 </html>
