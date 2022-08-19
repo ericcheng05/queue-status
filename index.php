5fd7c2049f6c9375d82b2f324e4a9624c7f379ee
ericcheng05 <13010622+ericcheng05@users.noreply.github.com>
2022-08-20 00:14:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
 			$sushiroStoreList = "https://sushipass.sushiro.com.hk/api/2.0/info/storelist?latitude=22&longitude=114&numresults=25&region=HK";
 			$sushiroQueuePath = "https://sushipass.sushiro.com.hk/api/2.0/remote/groupqueues?region=HK&storeid=";
 			echo "breakpoint";
+		//  Initiate curl
+$ch = curl_init();
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$sushiroStoreList);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
+		echo "breakpoint";
 			$StoreList = file_get_contents($sushiroStoreListPath);
   		$decodedStoreList = json_decode($StoreList);
 			$storeCount = count($decodedStoreList);
