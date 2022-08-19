210adf4e90d3bde258093486365376ea859c90c0
ericcheng05 <13010622+ericcheng05@users.noreply.github.com>
2022-08-20 00:05:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Sushiro HK Store Queue Status</title>
+    <meta http-equiv="refresh" content="120">
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php
+  $sushiroStoreList = "https://sushipass.sushiro.com.hk/api/2.0/info/storelist?latitude=22&longitude=114&numresults=25&region=HK";
+  $sushiroQueuePath = "https://sushipass.sushiro.com.hk/api/2.0/remote/groupqueues?region=HK&storeid=";
+  
+  $StoreList = file_get_contents($sushiroStoreListPath);
+  $decodedStoreList = json_decode($StoreList);
+  $storeCount = count($decodedStoreList);
+
+  for ($x = 0; $x < $storeCount; $x++)
+  {
+    echo $decodedStoreList[$x]->name;
+    echo " Current Queue Size: ".$decodedStoreList[$x]->waitingGroup;
+    
+    $StoreQueue = file_get_contents($sushiroQueuePath.$decodedStoreList[$x]->id);
+    $decodedStoreQueue = json_decode($StoreQueue);
+    echo "Next Ticket: ".$decodedStoreQueue->mixedQueue[0], PHP_EOL;;
+  }
+?>
   </body>
 </html>
