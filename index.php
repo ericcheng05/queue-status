12f4b3708a034f6fb58b079d087779a0afffcb3d
ericcheng05 <13010622+ericcheng05@users.noreply.github.com>
2022-08-20 13:38:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
 					//Add our normal / single cURL handle to the cURL multi handle.
 					curl_multi_add_handle($mh, $storeRequests[$k]['curl_handle']);
 				}
-				echo count($urls);
-				echo count($storeRequests);
+				// echo count($urls);
+				// echo count($storeRequests);
 
 				//Execute our requests using curl_multi_exec.
 				$stillRunning = false;
@@ -67,15 +67,15 @@
     					//Remove the handle from the multi handle.
     					curl_multi_remove_handle($mh, $request['curl_handle']);
 					//Get the response content and the HTTP status code.
-					$requests[$k]['content'] = curl_multi_getcontent($request['curl_handle']);
+					$storeRequests[$k]['content'] = curl_multi_getcontent($request['curl_handle']);
 					// $requests[$k]['http_code'] = curl_getinfo($request['curl_handle'], CURLINFO_HTTP_CODE);
 					//Close the handle.
-					curl_close($requests[$k]['curl_handle']);
+					curl_close($storeRequests[$k]['curl_handle']);
 				}
 				//Close the multi handle.
 				curl_multi_close($mh);
 				//var_dump the $requests array for example purposes.
-				var_dump($requests);
+				var_dump($storeRequests);
 			
 			
 			/*
