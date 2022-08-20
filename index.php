80ac49d3f870921459df2fd9d1d6d65108baf95c
ericcheng05 <13010622+ericcheng05@users.noreply.github.com>
2022-08-20 09:38:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?php
-	$sushiroStoreList = "https://sushipass.sushiro.com.hk/api/2.0/info/storelist?latitude=22&longitude=114&numresults=25&region=HK";
-	$sushiroQueuePath = "https://sushipass.sushiro.com.hk/api/2.0/remote/groupqueues?region=HK&storeid=";
-	//  Initiate curl
-	$channel = curl_init();
-	curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($channel, CURLOPT_URL,$sushiroStoreList);
-	// Execute
-	$storeList = curl_exec($channel);
-	curl_close($channel);
-
-	$array_StoreList = json_decode($storeList);
-	$array_allStoreQueueStatus = array();
-	foreach ($array_StoreList as $value) 
-	{
-		$channel = curl_init();
-		curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($channel, CURLOPT_URL,$sushiroQueuePath.$value->id);
-		// Execute
-		$storeQueue = curl_exec($channel);
-		$array_StoreQueue = json_decode($storeQueue);
-		curl_close($channel);
-		$array_Calling = $array_StoreQueue->mixedQueue;
-		echo $value->name;
-		echo $value->waitingGroup;
-		echo count($array_Calling);
-	}
-?>
-
 <html>
 	<head>
 		<title>Sushiro HK Store Queue Status</title>
@@ -43,15 +14,71 @@
 				<th>Next 3rd Ticket</th>
   			</tr>
 			<?php
+				$sushiroStoreList = "https://sushipass.sushiro.com.hk/api/2.0/info/storelist?latitude=22&longitude=114&numresults=25&region=HK";
+				$sushiroQueuePath = "https://sushipass.sushiro.com.hk/api/2.0/remote/groupqueues?region=HK&storeid=";
+				//  Initiate curl
+				$channel = curl_init();
+				curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($channel, CURLOPT_URL,$sushiroStoreList);
+				// Execute
+				$storeList = curl_exec($channel);
+				curl_close($channel);
+
+				$array_StoreList = json_decode($storeList);
+				$array_allStoreQueueStatus = array();
+				foreach ($array_StoreList as $value) 
+				{
+					$channel = curl_init();
+					curl_setopt($channel, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($channel, CURLOPT_URL,$sushiroQueuePath.$value->id);
+					// Execute
+					$storeQueue = curl_exec($channel);
+					$array_StoreQueue = json_decode($storeQueue);
+					curl_close($channel);
+					
+					echo "<tr>", PHP_EOL;
+					echo "<td>$value->name</td>", PHP_EOL;
+					echo "<td>$value->waitingGroup</td>", PHP_EOL;
+					if (count($array_StoreQueue->mixedQueue) == 3)
+					{
+						echo "<td>$array_StoreQueue->mixedQueue[0]</td>", PHP_EOL;
+						echo "<td>$array_StoreQueue->mixedQueue[1]</td>", PHP_EOL;
+						echo "<td>$array_StoreQueue->mixedQueue[2]</td>", PHP_EOL;
+					}
+					elseif (count(array_StoreQueue->mixedQueue) == 2)
+					{
+						echo "<td>$array_StoreQueue->mixedQueue[0]</td>", PHP_EOL;
+						echo "<td>$array_StoreQueue->mixedQueue[1]</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+					}
+					elseif (count(array_StoreQueue->mixedQueue) == 2)
+					{
+						echo "<td>$array_StoreQueue->mixedQueue[0]</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+					}
+					elseif (count(array_StoreQueue->mixedQueue) == 1)
+					{
+						echo "<td>$array_StoreQueue->mixedQueue[0]</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+					}
+					else
+					{
+						echo "<td>NA</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+						echo "<td>NA</td>", PHP_EOL;
+					}
+					echo "</tr>", PHP_EOL;					
+				}
+			?>
+
+			
+			
+			<?php
 				foreach ($allStoreQueueStatus as $value) 
 				{
-					echo "<tr>", PHP_EOL;
-					echo "<td>$value[0]</td>", PHP_EOL;
-					echo "<td>$value[1]</td>", PHP_EOL;
-					echo "<td>0</td>", PHP_EOL;
-					echo "<td>1</td>", PHP_EOL;
-					echo "<td>2</td>", PHP_EOL;
-					echo "</tr>", PHP_EOL;
+					
 				}
 			?>
 		</table>
